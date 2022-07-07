40a11331b9be4db855e7e5d6aca5eccbbcb741db
SOMYA RAWAT <100460439+codewithsom@users.noreply.github.com>
2022-07-07 15:53:09 +0530

    Add files via upload

---
+++ Day-4/2nd_Question/mul_div.js
@@ -0,0 +1,14 @@
+function multiplyBy()
+{
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+
+function divideBy() 
+{ 
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+} 
