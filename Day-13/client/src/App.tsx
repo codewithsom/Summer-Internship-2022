69d59d8a67462b8279a232397804fd1a93231a6e
SOMYA RAWAT <100460439+codewithsom@users.noreply.github.com>
2022-07-21 08:16:19 +0530

    Add files via upload

---
+++ Day-13/client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [visits, setVisits] = useState()
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get('http://localhost:8000')
+      setVisits(data)
+    })()
+  }, [])
+  return (
+    <>
+      <div className="main">
+        This Website has been visited {visits} times 😎!
+      </div>
+    </>
+  )
+}
+
+export default App;
